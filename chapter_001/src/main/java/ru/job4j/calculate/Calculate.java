ae3e7af0c03c09352efd754511f78950cf93bd7f
rmnick <r.m.nick@yandex.ru>
2018-07-29 17:22:44 +0500

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,20 @@
 package ru.job4j.calculate;
 
+/**
+* Calculator
+*
+* @author Rodionov Nick (r.m.nick@yandex.ru)
+* @version 1.0
+* @since 2018/07/29
+*/
+
 public class Calculate{
+	
+/**
+* Method main
+* @param args args
+*/
+package ru.job4j.calculate;
 	public static void main(String[] args){
 		System.out.println("Hello World");
     }		
