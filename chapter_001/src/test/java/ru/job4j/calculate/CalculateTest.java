79dca741d6c416dcb039de66c29eda30a65fba0e
rmnick <r.m.nick@yandex.ru>
2018-08-02 00:57:14 +0500

    commit ради commit(а)

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -5,22 +5,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Rodionov Nick (r.m.nick@yandex.ru)
-* @version 1.0
-* @since 2018/07/29
-*/
+ * Test.
+ *
+ * @author Rodionov Nick (r.m.nick@yandex.ru)
+ * @version 1.0
+ * @since 2018/07/29
+ */
 public class CalculateTest {
   /**
-  * Test echo.
-  */ @Test
+   * Test echo.
+   */ @Test
   public void whenTakeNameThenTreeEchoPlusName() {
     String input = "Nick Rodionov";
-    String expect = "Echo, echo, echo : Nick Rodionov"; 
+    String expect = "Echo, echo, echo : Nick Rodionov";
     Calculate calc = new Calculate();
     String result = calc.echo(input);
     assertThat(result, is(expect));
   }
- 
+
 }
