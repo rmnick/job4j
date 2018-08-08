25fb19710f2f2b21f53861d0dbe8802f569498ac
rmnick <r.m.nick@yandex.ru>
2018-08-08 21:01:07 +0500

    max number from three numbers

--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -16,8 +16,33 @@ public class MaxTest {
         assertThat(maximum.max(3, 5), is(5));
     }
     @Test
-    public void whenFirstEqualsSecondThenFirst() {
+    public void whenFirstEqualSecondThenFirst() {
         Max maximum = new Max();
         assertThat(maximum.max(6, 6), is(6));
+    }
+    @Test
+    public void whenFirstBiggerThenOthersThenFirst() {
+        Max maximum = new Max();
+        assertThat(maximum.max(2, 1, 0), is(2));
+    }
+    @Test
+    public void whenSecondBiggerThenOthersThenSecond() {
+        Max maximum = new Max();
+        assertThat(maximum.max(3, 5, 1), is(5));
+    }
+    @Test
+    public void whenThirdBiggerThenOthersThenThird() {
+        Max maximum = new Max();
+        assertThat(maximum.max(3, 5, 8), is(8));
+    }
+    @Test
+    public void whenFirstEqualSecondAndBothBiggerThenThirdThenFirst() {
+        Max maximum = new Max();
+        assertThat(maximum.max(6, 6, 4), is(6));
+    }
+    @Test
+    public void whenAllEqualThenFirst() {
+        Max maximum = new Max();
+        assertThat(maximum.max(9, 9, 9), is(9));
     }
 }
