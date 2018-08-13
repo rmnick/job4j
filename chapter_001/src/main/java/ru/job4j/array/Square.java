a5022fb737015d1b98fd0299048c12cfeb60bb50
rmnick <r.m.nick@yandex.ru>
2018-08-13 12:38:55 +0500

    fix Square

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -6,10 +6,10 @@ package ru.job4j.array;
  * @since 2018/08/11.
  */
 public class Square {
-    public static int[] calculate(int bound) {
+    public int[] calculate(int bound) {
         int[] arr = new int[bound];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] += Math.pow(i + 1, 2);
+            arr[i] = (int)Math.pow(i + 1, 2);
         }
         return arr;
     }
