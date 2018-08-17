75a62daf18b2c3956aca26fd09843b15ebd6e7bb
rmnick <r.m.nick@yandex.ru>
2018-08-17 15:05:37 +0500

    checking diagonals(fix)

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -4,14 +4,24 @@ public class MatrixCheck {
 
     public boolean checkMatrix(boolean[][] arr) {
         boolean rsl = true;
-        if (arr.length % 2 != 0) { //checking for independent diagonals
+       /*
+       if (arr.length % 2 != 0) { //checking for independent diagonals
             rsl = (arr[0][0] == arr[0][arr.length - 1]) && this.checkLeft(arr); //if first elements are different then false without checking, if not then checking only one diag.
         } else {
             rsl = this.checkLeft(arr) && this.checkRight(arr);
         }
-        return  rsl;
+        */
+        for (int i = 1; i < arr.length; i++) {
+            rsl = arr[0][0] == arr[i][i] && arr[0][arr.length - 1] == arr[i][arr.length - i - 1];
+            if (!rsl) {
+                break;
+            }
+        }
+        return rsl;
     }
-    private boolean checkLeft(boolean[][] arr) {
+
+ /*
+  private boolean checkLeft(boolean[][] arr) {
         boolean rsl = true;
         for (int i = 1; i < arr.length; i++) {
             rsl = arr[0][0] == arr[i][i];
@@ -32,4 +42,6 @@ public class MatrixCheck {
         }
         return rsl;
     }
+   */
 }
+
