08e46d25f65020a4a6368a299c0c7270c63595ff
rmnick <r.m.nick@yandex.ru>
2018-08-19 14:30:08 +0500

    TicTacToe(gibberish)

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -8,14 +8,147 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        return false;
+        //horizontal check
+        boolean rsl = false;
+        for (int i = 0; i < this.table.length; i++) {
+            if (rsl) {
+                return true;
+            }
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[i][j].hasMarkX()) {
+                    rsl = true;
+                } else {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
+        if (rsl) {
+            return  true;
+        }
+
+        //vertical check
+        for (int i = 0; i < this.table.length; i++) {
+            if (rsl) {
+                return  true;
+            }
+            for (int j = 0; j < this.table.length; j++) {
+                if (!this.table[j][i].hasMarkX()) {
+                    rsl = false;
+                    break;
+                } else {
+                    rsl = true;
+                }
+            }
+        }
+        if (rsl) {
+            return  true;
+        }
+
+        //diag check one
+        for (int i = 0; i < this.table.length; i++) {
+            if (!this.table[i][i].hasMarkX()) {
+                rsl = false;
+                break;
+            } else {
+                rsl = true;
+            }
+        }
+        if (rsl) {
+            return true;
+        }
+
+        //diag check two
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i][this.table.length - i - 1].hasMarkX()) {
+                rsl = true;
+            } else {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
     }
 
     public boolean isWinnerO() {
-        return false;
+        //horizont check
+        boolean rsl = false;
+        for (int i = 0; i < this.table.length; i++) {
+            if (rsl) {
+                return true;
+            }
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[i][j].hasMarkO()) {
+                    rsl = true;
+                } else {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
+        if (rsl) {
+            return  true;
+        }
+
+        //vertical check
+        for (int i = 0; i < this.table.length; i++) {
+            if (rsl) {
+                return true;
+            }
+            for (int j = 0; j < this.table.length; j++) {
+                if (this.table[j][i].hasMarkO()) {
+                    rsl = true;
+                } else {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
+        if (rsl) {
+            return  true;
+        }
+
+        //diag check one
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i][i].hasMarkO()) {
+                rsl = true;
+            } else {
+                rsl = false;
+                break;
+            }
+        }
+        if (rsl) {
+            return true;
+        }
+
+        //diag check two
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i][this.table.length - i - 1].hasMarkO()) {
+                rsl = true;
+            } else {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
     }
 
     public boolean hasGap() {
-        return true;
+        boolean rsl = false;
+        for (int i = 0; i < this.table.length; i++) {
+            if (rsl) {
+                break;
+            }
+            for (int j = 0; j < this.table.length; j++) {
+                if (!(this.table[i][j].hasMarkX() || this.table[i][j].hasMarkO())) {
+                    rsl = true;
+                    break;
+                } else {
+                    rsl = false;
+                }
+            }
+        }
+        return rsl;
     }
+
 }
--- chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
+++ chapter_001/src/test/java/ru/job4j/tictactoe/Logic3TTest.java
@@ -6,16 +6,54 @@ import static org.junit.Assert.assertThat;
 
 public class Logic3TTest {
     @Test
-    public void logicTest() {
-        Figure3T exmpl[][] = {
+    public void logicTestDiagX() {
+        Figure3T[][] exmpl = {
                 {new Figure3T(true), new Figure3T(), new Figure3T(true)},
-                {new Figure3T(true), new Figure3T(true), new Figure3T(true)},
-                {new Figure3T(true), new Figure3T(), new Figure3T(true)}
+                {new Figure3T(), new Figure3T(true), new Figure3T(true)},
+                {new Figure3T(), new Figure3T(), new Figure3T(true)}
         };
 
         Logic3T desk = new Logic3T(exmpl);
         assertThat(desk.isWinnerX(), is(true));
     }
 
+        @Test
+        public void logicTestDiagXBack() {
+            Figure3T[][] exmpl = {
+                    {new Figure3T(true), new Figure3T(), new Figure3T(true)},
+                    {new Figure3T(), new Figure3T(true), new Figure3T(true)},
+                    {new Figure3T(true), new Figure3T(), new Figure3T()}
+            };
 
+            Logic3T desk = new Logic3T(exmpl);
+            assertThat(desk.isWinnerX(), is(true));
+        }
+
+    @Test
+    public void logicTestHorizontX() {
+        Figure3T[][] exmpl = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(), new Figure3T(), new Figure3T(true)},
+                {new Figure3T(true), new Figure3T(true), new Figure3T(true)}
+        };
+
+        Logic3T desk = new Logic3T(exmpl);
+        assertThat(desk.isWinnerX(), is(true));
+    }
+
+    @Test
+    public void logicTestVerticalX() {
+        Figure3T[][] exmpl = {
+                {new Figure3T(true), new Figure3T(), new Figure3T()},
+                {new Figure3T(true), new Figure3T(true), new Figure3T()},
+                {new Figure3T(true), new Figure3T(), new Figure3T(true)}
+        };
+
+        Logic3T desk = new Logic3T(exmpl);
+        assertThat(desk.isWinnerX(), is(true));
+    }
 }
+
+
+
+
