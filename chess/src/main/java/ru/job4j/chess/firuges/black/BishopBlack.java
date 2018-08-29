a9a01ea165c71a01c9fa2ce18a5641c41612a606
rmnick <r.m.nick@yandex.ru>
2018-08-29 16:17:40 +0500

    start(chess)

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,43 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        Cell[] steps = new Cell[0];
+        for(int i = -7; i < 8; i++) {
+            if ((source.y == dest.y + i && source.x == dest.x + i)
+                    || (source.y == dest.y - i && source.x == dest.x + i)) {
+                steps = new Cell[] { dest };
+            }
+        }
+        return steps;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
