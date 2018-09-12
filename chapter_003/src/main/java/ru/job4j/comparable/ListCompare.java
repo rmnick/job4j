cc7d4c921944e39597a4679db0407c0ad88a0a17
rmnick <r.m.nick@yandex.ru>
2018-09-12 18:47:28 +0500

    String comparator

---
+++ chapter_003/src/main/java/ru/job4j/comparable/ListCompare.java
@@ -0,0 +1,19 @@
+package ru.job4j.comparable;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int length = left.length() <= right.length() ? left.length() : right.length();
+        for (int i = 0; i < length; i++) {
+            if (result == 0) {
+                result = Character.compare(left.charAt(i), right.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return result != 0 ? result : Integer.compare(left.length(), right.length());
+    }
+}
