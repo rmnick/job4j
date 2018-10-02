031bbab774ed314bda4a3ea5c78c4c0f9d187e3c
rmnick <r.m.nick@yandex.ru>
2018-10-02 13:18:45 +0500

    working with Iterator(step1)

--- chapter_005/src/main/java/iterator/Converter.java
+++ chapter_005/src/main/java/iterator/Converter.java
@@ -6,16 +6,18 @@ import java.util.NoSuchElementException;
 public class Converter {
     public Iterator<Integer> convert(final Iterator<Iterator<Integer>> iter) {
         return new Iterator<Integer>() {
-            Iterator index = iter.next();
+            private boolean flag = false;
+            private Iterator index = iter.next();
             @Override
             public boolean hasNext() {
-                return iter.hasNext() || index.hasNext();
+               return flag ? index.hasNext() || iter.hasNext() : index.hasNext();
             }
 
             @Override
-            public Integer next() throws NoSuchElementException{
+            public Integer next() throws NoSuchElementException {
+                flag = true;
                 if (!this.hasNext()) {
-                    throw new NoSuchElementException();
+                   throw new NoSuchElementException();
                 }
                 if (!index.hasNext()) {
                     index = iter.next();
