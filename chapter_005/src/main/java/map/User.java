38ba3ee605651c5406acb2e5595db7b327a82447
rmnick <r.m.nick@yandex.ru>
2018-10-10 03:37:12 +0500

    Map(with equals without hashCode)

--- chapter_005/src/main/java/map/User.java
+++ chapter_005/src/main/java/map/User.java
@@ -14,11 +14,25 @@ public class User {
     }
 
     public int hashCode() {
-        int result = 17;
+        /*int result = 17;
         result = 31 * result + name.hashCode();
         result = 31 * result + Integer.valueOf(children).hashCode();
         result = 31 * result + birthday.hashCode();
-        return result;
+        return result;*/
+        return super.hashCode();
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return this.children == user.getChildren()
+                && this.name.equals(user.getName())
+                && this.birthday.equals(user.getBirthday());
     }
 
     public String getName() {
