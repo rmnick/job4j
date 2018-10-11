b53233ed0cf353ba7af29bb0ac3c7a010e15b4ca
rmnick <r.m.nick@yandex.ru>
2018-10-12 01:08:52 +0500

    CustomHashMap(step1)

---
+++ chapter_005/src/main/java/map/CustomHashMap.java
@@ -0,0 +1,214 @@
+package map;
+
+import java.util.*;
+
+public class CustomHashMap<K, V> implements Iterable<Map.Entry<K, V>> {
+    private Entry[] table;
+    private final float loadFactor = 0.6f;
+    private static final int MAX_CAPACITY = 1 << 30;
+    private int size = 0;
+    private int modCount = 0;
+
+    public int getTableLength() {
+        return table.length;
+    }
+    public int getSize() {
+        return this.size;
+    }
+
+    public List<V> getValues() {
+        List<V> values = new ArrayList<>();
+        for (Entry<K, V> e : table) {
+            if (e != null) {
+                values.add(e.getValue());
+            }
+        }
+        return values;
+    }
+
+    public Set<K> getKeys() {
+        Set<K> keys = new HashSet<>();
+        for (Entry<K, V> e : table) {
+            if (e != null) {
+                keys.add(e.getKey());
+            }
+        }
+        return keys;
+    }
+
+
+    public CustomHashMap() {
+        this.table = new Entry[16];
+    }
+
+    private static class Entry<K, V> implements Map.Entry<K, V> {
+        K key;
+        V value;
+        int hash;
+
+        Entry(K key, V value, int hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue(V value) {
+            this.value = value;
+            return value;
+        }
+
+        int getHash() {
+            return hash;
+        }
+    }
+
+    private int hash(int h) {
+        h = h ^ (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
+    }
+
+    private int indexFor(int h, int length) {
+        return h & (length - 1);
+    }
+
+    private boolean put(K key, V value) {
+        int h = hash(key.hashCode());
+        int index = indexFor(h, table.length);
+        if (table[index] != null) {
+            return false;
+        } else {
+            table[index] = new Entry(key, value, h);
+            size++;
+            modCount++;
+            return true;
+        }
+    }
+
+    private Entry[] resize() {
+        Entry[] temp = new Entry[this.table.length * 2];
+        temp[0] = table[0];
+        for (int i = 1; i < this.table.length; i++) {
+            if (table[i] != null) {
+                if (temp[indexFor(table[i].getHash(), temp.length)] != null) {
+                    for (int j = 1; j < temp.length; j++) {
+                        if (temp[j] == null) {
+                            temp[j] = table[i];
+                            break;
+                        }
+                    }
+                } else {
+                    temp[indexFor(table[i].getHash(), temp.length)] = table[i];
+                }
+            }
+        }
+        return temp;
+    }
+
+    boolean insert(K key, V value) {
+        if (size == MAX_CAPACITY) {
+            return false;
+        }
+        if (key == null) {
+            if (table[0] == null) {
+                table[0] = new Entry(key, value, 0);
+                size++;
+                modCount++;
+                return true;
+            }
+            return false;
+        }
+        if (table.length * loadFactor > size) {
+            return put(key, value);
+        } else {
+            table = this.resize();
+            return put(key, value);
+        }
+    }
+
+    public V get(K key) {
+        V result = null;
+        Entry<K, V> e = null;
+        if (key == null) {
+            if (table[0] != null) {
+                e = table[0];
+                result = e.getValue();
+            }
+        } else {
+            int h = hash(key.hashCode());
+            int index = indexFor(h, table.length);
+            if (table[index] != null) {
+                e = table[index];
+                if (e.getKey().equals(key)) {
+                result = e.getValue();
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean delete(K key) {
+        boolean result = false;
+        Entry<K, V> e = null;
+        if (key == null) {
+            if (table[0] != null) {
+                table[0] = null;
+                size--;
+                modCount++;
+                result = true;
+            }
+        } else {
+            int h = hash(key.hashCode());
+            int index = indexFor(h, table.length);
+            if (table[index] != null) {
+                e = table[index];
+                if (e.getKey().equals(key)) {
+                    table[index] = null;
+                    size--;
+                    modCount++;
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public Iterator<Map.Entry<K, V>> iterator() {
+        return new Iterator<Map.Entry<K, V>>() {
+            int count = 0;
+            int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() throws ConcurrentModificationException {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return count < size;
+            }
+            @Override
+            public Entry<K, V> next() throws NoSuchElementException {
+                Entry<K, V> e = null;
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                for (int i = count; i < table.length; i++) {
+                    if (table[i] != null) {
+                        //count = i;
+                        //count++;
+                        e = table[i];
+                        count = i + 1;
+                        break;
+                    }
+                }
+                return e;
+            }
+        };
+    }
+
+}
---
+++ chapter_005/src/test/java/map/CustomHashMapTest.java
@@ -0,0 +1,109 @@
+package map;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static  org.hamcrest.Matchers.is;
+
+public class CustomHashMapTest {
+    @Test
+    public void whenInsertEqualKeysThenFalse() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        assertThat(map.insert(3, "c"), is(true));
+        assertThat(map.insert(2, "b"), is(false));
+    }
+    @Test
+    public void whenInsertDifferentKeysWithSameHashThenFalse() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        assertThat(map.insert(5, "e"), is(true));
+        assertThat(map.insert(6, "f"), is(true));
+        assertThat(map.insert(66, "dummy"), is(false));
+    }
+    @Test
+    public void whenInsertManyKeysThenSizeIncrease() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        map.insert(4, "d");
+        map.insert(5, "e");
+        map.insert(6, "f");
+        assertThat(map.getTableLength(), is(16));
+        map.insert(7, "g");
+        map.insert(8, "h");
+        map.insert(9, "k");
+        map.insert(10, "j");
+        map.insert(11, "i");
+        map.insert(12, "m");
+        assertThat(map.getTableLength(), is(32));
+        assertThat(map.getValues().toString(), is("[a, b, c, d, e, f, g, h, k, j, i, m]"));
+    }
+    @Test
+    public void whenGetRightKeyThenValue() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        assertThat(map.get(2), is("b"));
+    }
+    @Test
+    public void whenGetWrongKeyThenNull() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        assertThat(map.get(4) == null, is(true));
+    }
+    @Test
+    public void whenGetWrongKeyWithExistHashThenNull() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(6, "f");
+        assertThat(map.get(66) == null, is(true));
+    }
+    @Test
+    public void whenDeleteRightKeyThenRemoveValueAndTrue() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        map.insert(4, "d");
+        assertThat(map.delete(3), is(true));
+        assertThat(map.getValues().toString(), is("[a, b, d]"));
+    }
+    @Test
+    public void whenDeleteWrongKeyThenFalse() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        assertThat(map.delete(4), is(false));
+    }
+    @Test
+    public void whenIteratorHasNextThenNext() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        map.insert(1, "a");
+        map.insert(2, "b");
+        map.insert(3, "c");
+        Iterator<Map.Entry<Integer, String>> iterator = map.iterator();
+        assertThat(iterator.next().getValue(), is("a"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next().getValue(), is("b"));
+       // assertThat(iterator.next().getValue(), is("c"));
+       // assertThat(iterator.hasNext(), is(false));
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void whenIteratorHasNotNextThenException() {
+        CustomHashMap<Integer, String> map = new CustomHashMap<>();
+        Iterator<Map.Entry<Integer, String>> iterator = map.iterator();
+        iterator.next();
+    }
+}
