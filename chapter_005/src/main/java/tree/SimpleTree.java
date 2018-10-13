d4f7f49a94c6798c7420d437bb9aa4c3c7e900b2
rmnick <r.m.nick@yandex.ru>
2018-10-13 17:23:20 +0500

    CustomTree

---
+++ chapter_005/src/main/java/tree/SimpleTree.java
@@ -0,0 +1,68 @@
+package tree;
+
+import java.util.*;
+
+public class SimpleTree<E extends Comparable<E>> implements Iterable<E> {
+
+    Node<E> root;
+
+    public SimpleTree(E root) {
+        this.root = new Node(root);
+    }
+
+    public boolean add(E parent, E child) {
+        return true;
+    }
+
+    private static class Node<E extends Comparable<E>> {
+        private final List<Node<E>> children = new ArrayList<>();
+        private final E value;
+
+        public Node(final E value) {
+            this.value = value;
+        }
+
+        public void add(Node<E> child) {
+            this.children.add(child);
+        }
+
+        public List<Node<E>> leaves() {
+            return this.children;
+        }
+
+        public boolean eqValue(E that) {
+            return this.value.compareTo(that) == 0;
+        }
+    }
+
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
+    }
+}
---
+++ chess/src/test/java/ru/job4j/chess/figures/black/LogicTest.java
@@ -0,0 +1,4 @@
+package ru.job4j.chess.figures.black;
+
+public class LogicTest {
+}
