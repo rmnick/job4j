730efc267a212ad205c1de66cc68a3f91e7f1f87
rmnick <r.m.nick@yandex.ru>
2018-10-19 23:25:37 +0500

    Algorithms(step2)

--- chapter_005/src/main/java/algorithms/Sort.java
+++ chapter_005/src/main/java/algorithms/Sort.java
@@ -15,15 +15,37 @@ public class Sort {
         }
     }
 
-    public static void choiceSort(int[] arr) {
-        int temp;
+    public static void selectedSort(int[] arr) {
+        int temp = arr[0];
+        int index = 0;
+        boolean flag = false;
         for (int i = 0; i < arr.length; i++) {
+            if (flag) {
+                arr[index] = arr[i - 1];
+                arr[i - 1] = temp;
+                flag = false;
+            }
+            temp = arr[i];
             for (int j = i; j < arr.length - 1; j++) {
-                if (arr[j + 1] < arr[i]) {
+                if (arr[j + 1] < temp) {
                     temp = arr[j + 1];
-                    arr[j + 1] = arr[i];
-                    arr[i] = temp;
+                    index = j + 1;
+                    flag = true;
                 }
+            }
+        }
+    }
+
+    public static void insertedSort(int[] arr) {
+        int temp;
+        int j;
+        for (int i = 1; i < arr.length; i++) {
+            temp = arr[i];
+            j = i;
+            while (j > 0 && arr[j - 1] > temp) {
+                arr[j] = arr[j - 1];
+                j--;
+                arr[j] = temp;
             }
         }
     }
--- chapter_005/src/test/java/algorithms/SortTests.java
+++ chapter_005/src/test/java/algorithms/SortTests.java
@@ -28,15 +28,31 @@ public class SortTests {
     }
 
     @Test
-    public void choiceSortTesting() {
+    public void selectedSortTesting() {
         int[] arrEven = new int[] {3, 1, 0, 90, 11, 12, 5, 2};
-        Sort.choiceSort(arrEven);
+        Sort.selectedSort(arrEven);
         List<Integer> list = Arrays.stream(arrEven).boxed().collect(Collectors.toList());
         StringJoiner str = new StringJoiner(" ", "", "");
         list.stream().map(num -> String.valueOf(num)).forEach(str :: add);
         assertThat(str.toString(), is("0 1 2 3 5 11 12 90"));
         int[] arrOdd = new int[] {3, 1, 6, 0, 90, 6, 11, 12, 5, 2, 0};
-        Sort.choiceSort(arrOdd);
+        Sort.selectedSort(arrOdd);
+        list = Arrays.stream(arrOdd).boxed().collect(Collectors.toList());
+        str = new StringJoiner(" ", "", "");
+        list.stream().map(num -> String.valueOf(num)).forEach(str :: add);
+        assertThat(str.toString(), is("0 0 1 2 3 5 6 6 11 12 90"));
+    }
+
+    @Test
+    public void insertedSortTesting() {
+        int[] arrEven = new int[] {3, 1, 0, 90, 11, 12, 5, 2};
+        Sort.insertedSort(arrEven);
+        List<Integer> list = Arrays.stream(arrEven).boxed().collect(Collectors.toList());
+        StringJoiner str = new StringJoiner(" ", "", "");
+        list.stream().map(num -> String.valueOf(num)).forEach(str :: add);
+        assertThat(str.toString(), is("0 1 2 3 5 11 12 90"));
+        int[] arrOdd = new int[] {3, 1, 6, 0, 90, 6, 11, 12, 5, 2, 0};
+        Sort.insertedSort(arrOdd);
         list = Arrays.stream(arrOdd).boxed().collect(Collectors.toList());
         str = new StringJoiner(" ", "", "");
         list.stream().map(num -> String.valueOf(num)).forEach(str :: add);
