71066d0d689af2c2aed83cfb29e33dbe1eb7594f
rmnick <r.m.nick@yandex.ru>
2018-10-30 23:35:21 +0500

    fix stop consumer without producer

--- chapter_006/src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ chapter_006/src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -9,25 +9,21 @@ public class ParallelSearch {
         final int poisonPill = -100;
         final Thread producer = new Thread(
                 () -> {
-                    for (int index = 0; index != 3; index++) {
-                        try {
-                            queue.offer(index);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
                     try {
-                        queue.offer(poisonPill);
+                        for (int index = 0; index != 3; index++) {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } finally {
+                        try {
+                            queue.offer(poisonPill);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-
         );
         final Thread consumer = new Thread(
                 () -> {
