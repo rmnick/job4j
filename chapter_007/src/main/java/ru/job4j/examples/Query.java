cdb2157950f425fad316a5218e4f3bcb01c2a957
rmnick <r.m.nick@yandex.ru>
2018-12-07 19:08:20 +0500

    JDBC(test2)

--- chapter_007/src/main/java/ru/job4j/examples/Query.java
+++ chapter_007/src/main/java/ru/job4j/examples/Query.java
@@ -1,17 +1,39 @@
 package ru.job4j.examples;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import org.postgresql.core.SqlCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
+
+import java.sql.*;
 
 public class Query {
-    public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/car_catalog?user=postgres&password=password&ssl=true";
-        Connection conn = DriverManager.getConnection(url);
-       // Statement st = conn.createStatement();
-        System.out.println(conn.isValid(500));
-        conn.close();
-
+    private final static Logger LOG = LoggerFactory.getLogger(Query.class);
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/test";
+        String username = "postgres";
+        String password = "123";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+            PreparedStatement st = conn.prepareStatement("SELECT p.id, p.name, p.number FROM products p left outer join types t on p.id_type = t.id where t.name = (?);");
+            st.setString(1, "milk");
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                System.out.println(String.format("%d %s %d", rs.getInt("id"), rs.getString("name"), rs.getInt("number")));
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        } finally {
+            if (conn != null) {
+               try {
+                   conn.close();
+               } catch (SQLException e) {
+                   LOG.error(e.getMessage(), e);
+               }
+            }
+        }
     }
 }
