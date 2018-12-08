8ed821f0168d2a0cab11d0f29250dd25210e79fa
rmnick <r.m.nick@yandex.ru>
2018-12-08 18:00:30 +0500

    JDBC(test3)

--- chapter_007/src/main/java/ru/job4j/examples/Query.java
+++ chapter_007/src/main/java/ru/job4j/examples/Query.java
@@ -1,28 +1,38 @@
 package ru.job4j.examples;
 
-import org.postgresql.core.SqlCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.rmi.runtime.Log;
 
 import java.sql.*;
 
 public class Query {
     private final static Logger LOG = LoggerFactory.getLogger(Query.class);
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/test";
-        String username = "postgres";
-        String password = "123";
+        String url = "jdbc:postgresql://localhost:5432/products?user=postgres&password=password";
+        //String username = "postgres";
+        //String password = "password";
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(url, username, password);
-            PreparedStatement st = conn.prepareStatement("SELECT p.id, p.name, p.number FROM products p left outer join types t on p.id_type = t.id where t.name = (?);");
-            st.setString(1, "milk");
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                System.out.println(String.format("%d %s %d", rs.getInt("id"), rs.getString("name"), rs.getInt("number")));
-            }
-            rs.close();
+            conn = DriverManager.getConnection(url/*, username, password*/);
+            //PreparedStatement st = conn.prepareStatement("SELECT p.id, p.name, p.number FROM products p left outer join types t on p.id_type = t.id where t.name = (?);");
+            //st.setString(1, "milk");
+            //ResultSet rs = st.executeQuery();
+            //while (rs.next()) {
+            //    System.out.println(String.format("%d %s %d", rs.getInt("id"), rs.getString("name"), rs.getInt("number")));
+            //}
+            //rs.close();
+            //Statement st = conn.createStatement();
+           // boolean result = st.execute("create table manager (id serial primary key, name varchar(200), position varchar(200), date_start timestamp);");
+           // System.out.println(result);
+            //PreparedStatement st = conn.prepareStatement("insert into manager (name, position, date_start) values (?, ?, ?);");
+            //st.setString(1, "Nick");
+           //st.setString(2, "boss");
+           //st.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+            //st.execute();
+            //System.out.println("is done");
+            Statement st = conn.createStatement();
+            st.execute("drop table manager;");
+            System.out.println("is done");
             st.close();
         } catch (Exception e) {
             LOG.error(e.getMessage(), e);
