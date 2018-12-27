35fa37ae416db7dab4fb4f3c59a728a9f3f704db
rmnick <r.m.nick@yandex.ru>
2018-12-27 16:37:37 +0500

    IO(first)

---
+++ chapter_008/src/main/java/ru/job4j/even/EvenChecker.java
@@ -0,0 +1,81 @@
+package ru.job4j.even;
+
+
+import java.io.*;
+
+public class EvenChecker {
+
+    public boolean findEvenSimbol(InputStream in) {
+        boolean result = false;
+        try {
+            int data = in.read();
+            while (data != -1) {
+                char c = (char) data;
+                if (Character.isDigit(c)
+                        && Integer.parseInt(String.valueOf(c)) != 0
+                        && Integer.parseInt(String.valueOf(c)) % 2 == 0) {
+                    result = true;
+                    break;
+                }
+                data = in.read();
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public boolean findEvenNumber(InputStream in) {
+        boolean result = false;
+        StringBuilder str = new StringBuilder();
+        try {
+            int data = in.read();
+            while (data != -1) {
+                char c = (char) data;
+                if (Character.isDigit(c)) {
+                    str.append(c);
+                } else {
+                    if (str.length() != 0
+                            && Integer.parseInt(str.toString()) != 0
+                            && Integer.parseInt(str.toString()) % 2 == 0) {
+                        result = true;
+                        break;
+                    }
+                    str = new StringBuilder();
+                }
+                data = in.read();
+            }
+        } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return result;
+    }
+/*
+    public boolean findEvenNumberTwo(InputStream in) {
+        boolean result = false;
+        boolean flag = false;
+        try {
+            int data = in.read();
+            while (data != -1) {
+                char digit = 1;
+                char c = (char) data;
+                if (Character.isDigit(c)) {
+                    digit = c;
+                    flag = true;
+                } else {
+                    flag = false;
+                    }
+                if (!flag && Integer.parseInt(String.valueOf(digit)) % 2 == 0) {
+
+                }
+                data = in.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    */
+
+ }
