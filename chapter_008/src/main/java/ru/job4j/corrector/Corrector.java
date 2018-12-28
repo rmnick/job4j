27f14752f1a87dee1834efb5d35f613a42127b5e
rmnick <r.m.nick@yandex.ru>
2018-12-29 01:02:47 +0500

    IO(abuse)

---
+++ chapter_008/src/main/java/ru/job4j/corrector/Corrector.java
@@ -0,0 +1,30 @@
+package ru.job4j.corrector;
+
+import java.io.*;
+
+public class Corrector {
+/*
+    public static String[] abuse = {"fuck", "cunt"};
+
+    public static void correct(InputStream in, OutputStream out, String[] abuse) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            char c;
+            StringBuilder str = new StringBuilder();
+            while ((c = (char)in.read()) != -1) {
+                if (c != ' ') {
+                    str.append(c);
+                } else if (str.length() != 0) {
+                    for (String s : abuse) {
+                        if (!s.equals(str.toString())) {
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    */
+}
