9c62bd84d7a1ae2977a08b30dc376442592a6936
rmnick <r.m.nick@yandex.ru>
2018-12-29 15:20:03 +0500

    IO(lazy third :)

--- chapter_008/src/main/java/ru/job4j/corrector/Corrector.java
+++ chapter_008/src/main/java/ru/job4j/corrector/Corrector.java
@@ -3,28 +3,34 @@ package ru.job4j.corrector;
 import java.io.*;
 
 public class Corrector {
-/*
+
+    public static void main(String[] args) {
+        String str = "fdkjfhskd dhfsdfhsdfh dsfhksdhfkhsdk fdsdjfhkhsdkf";
+        try (InputStream in = new ByteArrayInputStream(str.getBytes());
+             OutputStream out = new ByteArrayOutputStream()) {
+            correct(in, out, abuse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static String[] abuse = {"fuck", "cunt"};
 
     public static void correct(InputStream in, OutputStream out, String[] abuse) {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             char c;
-            StringBuilder str = new StringBuilder();
-            while ((c = (char)in.read()) != -1) {
-                if (c != ' ') {
-                    str.append(c);
-                } else if (str.length() != 0) {
-                    for (String s : abuse) {
-                        if (!s.equals(str.toString())) {
-                        }
-                    }
-                }
+            String str = br.readLine();
+            while (str != null) {
+                out.write(str.getBytes());
+               System.out.println(out);
+               str = br.readLine();
             }
+            br.close();
         } catch (IOException e) {
 
         }
     }
 
-    */
+
 }
