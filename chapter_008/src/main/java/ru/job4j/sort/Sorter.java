f4c021247f470446642e33c4f23f47f300961fb5
rmnick <r.m.nick@yandex.ru>
2019-01-09 16:36:40 +0500

    sorter(Merge partTwo)

--- chapter_008/src/main/java/ru/job4j/sort/Sorter.java
+++ chapter_008/src/main/java/ru/job4j/sort/Sorter.java
@@ -5,96 +5,99 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Sorter {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        RandomAccessFile out = new RandomAccessFile("C:\\projects\\job4j\\chapter_008\\out.txt", "rw");
-        RandomAccessFile raf = new RandomAccessFile(new File("C:\\projects\\job4j\\chapter_008\\raf.txt"), "rw");
-        long pointer = 0;
-        List<RandomAccessFile> list = new ArrayList<>();
- /*       Set<String> storages = new TreeSet<>(new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() == o2.length() ? 0 : (o1.length() > o2.length() ? 1 : -1);
-            }
-        });*/
-        String[] storage = new String[10];
-        for (int i = 0; i < 50; i++) {
-            int n = (int) (Math.random() * 30);
-            for (int j = 0; j <= n; j++) {
-                raf.writeChars(String.valueOf('a'));
-            }
-            raf.writeChars(String.valueOf("\n"));
-        }
-        // raf.close();
-        raf.seek(0);
-        int countFile = 0;
-        RandomAccessFile temp = null;
-        while (pointer < raf.length()) {
-            //raf.seek(pointer);
-            int count = 0;
-            //storage.clear();
-            raf.seek(pointer);
-            while (count < 10 && pointer < raf.length()) {
-                String tempI = (raf.readLine());
-                //System.out.println(tempI);
-                storage[count] = (tempI);
-                pointer = raf.getFilePointer();
-                count++;
-            }
-            Arrays.sort(storage, new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return o1.length() == o2.length() ? 0 : (o1.length() > o2.length() ? 1 : -1);
+    public static void main(String[] args) {
+        String namePath = "/home/nick/work/job4j/chapter_008/";
+        try (RandomAccessFile out = new RandomAccessFile(String.format("%sout.txt", namePath), "rw");
+             RandomAccessFile source = new RandomAccessFile(String.format("%ssource.txt", namePath), "rw")) {
+            int sizeOfsource = 53;
+            long pointer = 0;
+            int sizeOfSegment = 5;
+            List<RandomAccessFile> list = new ArrayList<>();
+            List<String> storage = new ArrayList();
+            /**
+             * fill source file
+             */
+            for (int i = 0; i < sizeOfsource; i++) {
+                StringBuilder str = new StringBuilder();
+                int n = (int) (Math.random() * 30);
+                for (int j = 0; j <= n; j++) {
+                    str.append("a");
                 }
-            });
-
-
-            // System.out.println("!!!!!!!!!!!!");
-            temp = new RandomAccessFile(new File(String.format("C:/projects/job4j/chapter_008/%d.txt", countFile)), "rw");
-           // Iterator<String> iter = storage.iterator();
-            temp.seek(0);
-            for (int i = 0; i < storage.length; i++) {
-                //System.out.println(iter.next());
-                temp.writeChars(storage[i] + "\n");
+                source.write((str.toString() + System.lineSeparator()).getBytes());
             }
-            countFile++;
-            //list.add(temp);
-        }
-        long[] point = new long[countFile];
-        for (int i = 0; i < countFile; i++) {
-            list.add(new RandomAccessFile(String.format("C:/projects/job4j/chapter_008/%d.txt", i), "rw"));
-            //out.writeChars(list.get(i).readLine() + "\n");
-        }
-        //raf.close();
-        String shablon = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-        int counter = 0;
-        int flag = 0;
-        while (counter < 50) {
-            for (int i = 0; i < countFile; i++) {
-                temp = list.get(i);
-                point[i] = temp.getFilePointer();
-                String s = temp.readLine();
-                if (s.length() < shablon.length()) {
-                    shablon = s;
-                    flag = i;
+            /**
+             * split source file on small sorted segments
+             */
+            source.seek(0);
+            int countSegments = 0;
+            RandomAccessFile temp = null;
+            while (pointer < source.length()) {
+                int count = 0;
+                while (count < sizeOfSegment && pointer < source.length()) {
+                    String tempI = (source.readLine());
+                    storage.add(count, tempI);
+                    pointer = source.getFilePointer();
+                    count++;
                 }
-                //list.add(new RandomAccessFile(String.format("C:/projects/job4j/chapter_008/%d.txt", i), "rw"));
-                //out.writeChars(list.get(i).readLine() + "\n");
-                counter++;
-            }
-            for (int j = 0; j < countFile; j++) {
-                if (j != flag) {
-                    list.get(j).seek(point[j]);
+
+                storage = storage.stream().sorted(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o1.length() == o2.length() ? 0 : (o1.length() > o2.length() ? 1 : -1);
+                    }
+                }).collect(Collectors.toList());
+
+                temp = new RandomAccessFile(new File(String.format("%s%d.txt", namePath, countSegments)), "rw");
+                temp.seek(0);
+                for (int i = 0; i < storage.size(); i++) {
+                    temp.write((storage.get(i) + System.lineSeparator()).getBytes());
                 }
+                countSegments++;
+                storage.clear();
+                //list.add(temp);
+               // temp.close();
+            }
+            /**
+             * make list of segments and array of their pointers
+             */
+            long[] pointerOfSegments = new long[countSegments];
+            for (int i = 0; i < countSegments; i++) {
+                list.add(new RandomAccessFile(String.format("%s%d.txt", namePath, i), "rw"));
+            }
+            /**
+             * merge all segments in one file
+             */
+            int flag = 0;
+            while (out.length() < source.length()) {
+                String shablon = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                for (int i = 0; i < countSegments; i++) {
+                    temp = list.get(i);
+                    pointerOfSegments[i] = temp.getFilePointer();
+                    String s = temp.readLine();
+                    if (s != null && s.length() < shablon.length()) {
+                        shablon = s;
+                        flag = i;
+                    }
+                }
+                out.write((shablon + System.lineSeparator()).getBytes());
+                /**
+                 * put the pointer on right place
+                 */
+                for (int j = 0; j < countSegments; j++) {
+                    if (j != flag) {
+                        list.get(j).seek(pointerOfSegments[j]);
+                    }
+                }
+
             }
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        raf.close();
-        out.close();
     }
-        //raf.close();
-       // temp.close();
+
 }
