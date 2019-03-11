d472f6631a641dceca1af83b0bed6556da1efd4a
rmnick <r.m.nick@yandex.ru>
2019-03-11 20:01:44 +0500

    Servlet(hello world)

---
+++ chapter_11/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,21 @@
+package ru.job4j.servlets;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EchoServlet extends HttpServlet {
+    //private static final Logger LOG = LogManager.getLogger(EchoServlet.class.getName());
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(res.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
