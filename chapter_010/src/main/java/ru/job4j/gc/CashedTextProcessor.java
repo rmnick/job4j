001f32c6e9aeba5261a21187fc17d0968cdf6f00
rmnick <r.m.nick@yandex.ru>
2019-06-01 11:17:29 +0500

    fix resources Path, add comments

--- chapter_010/src/main/java/ru/job4j/gc/CashedTextProcessor.java
+++ chapter_010/src/main/java/ru/job4j/gc/CashedTextProcessor.java
@@ -3,11 +3,7 @@ package ru.job4j.gc;
 import java.io.File;
 import java.io.IOException;
 import java.lang.ref.SoftReference;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +11,7 @@ import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class CashedTextProcessor {
-    public static final URL PATH = CashedTextProcessor.class.getResource("/textfiles/");
+    public static final String PATH = CashedTextProcessor.class.getResource("/textfiles/").getPath();
     public static final String EXIT = "exit";
     public final Map<String, String> cash = new HashMap<>();
 
@@ -26,25 +22,28 @@ public class CashedTextProcessor {
         System.out.println("input file name");
         String result;
         do {
+            //validated answer is a key for map
             answer = sc.nextLine().toLowerCase().trim();
             if (answer.toLowerCase().trim().equals(EXIT)) {
                 break;
             }
             try {
-//                System.out.println(Paths.get(String.format("%s%s.txt", PATH, answer)));
-                File file = new File(String.format("%s%s.txt", PATH.getPath(), answer));
+                File file = new File(String.format("%s%s.txt", PATH, answer));
                 if (file.exists() && !file.isDirectory()) {
+                    //check reference existing
                     result = pr.cash.get(answer);
                     if (result == null) {
                         System.out.println("from file: ");
                         StringJoiner sj = new StringJoiner(System.lineSeparator());
-                        Files.lines(Paths.get(String.format("%s%s.txt", PATH.getPath(), answer)), StandardCharsets.UTF_8).forEach(line -> sj.add(line));
+                        Files.lines(Paths.get(String.format("%s%s.txt", PATH, answer))).forEach(line -> sj.add(line));
                         String value = sj.toString();
+                        //create softReference as value for map
                         SoftReference<String> sr = new SoftReference<>(value);
                         pr.cash.put(answer, sr.get());
                         result = value;
                     } else {
                         System.out.println("from cash: ");
+                        //get strong reference from soft
                         result = pr.cash.get(answer);
                     }
                     System.out.println(result);
