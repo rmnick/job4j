1e4ddd88ed47fa4476797255597ba1f3acfbedec
rmnick <r.m.nick@yandex.ru>
2019-06-05 18:40:53 +0500

    split cache method and main

--- chapter_010/src/main/java/ru/job4j/gc/CachedTextProcessor.java
+++ chapter_010/src/main/java/ru/job4j/gc/CachedTextProcessor.java
@@ -10,13 +10,35 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
-public class CashedTextProcessor {
-    public static final String PATH = CashedTextProcessor.class.getResource("/textfiles/").getPath();
+public class CachedTextProcessor {
+    public static final String PATH = CachedTextProcessor.class.getResource("/textfiles/").getPath();
     public static final String EXIT = "exit";
-    public final Map<String, SoftReference<String>> cash = new HashMap<>();
+    private final Map<String, SoftReference<String>> cache = new HashMap<>();
+
+    /**
+     * read txt file from resources or from cash
+     */
+    public String cacheText(String answer) throws IOException {
+        String result;
+        if (this.cache.get(answer) == null) {
+            //read from file, cache string
+            StringJoiner sj = new StringJoiner(System.lineSeparator());
+            Files.lines(Paths.get(String.format("%s%s.txt", PATH, answer))).forEach(line -> sj.add(line));
+            String value = sj.toString();
+            //create softReference as value for map and put entry<answer, softReference> to map
+            SoftReference<String> sr = new SoftReference<>(value);
+            this.cache.put(answer, sr);
+            result = value;
+        } else {
+            //read from cache
+            System.out.println("from cache");
+            result = this.cache.get(answer).get();
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
-        CashedTextProcessor pr = new CashedTextProcessor();
+        CachedTextProcessor cp = new CachedTextProcessor();
         Scanner sc = new Scanner(System.in);
         String answer;
         System.out.println("input file name");
@@ -30,21 +52,7 @@ public class CashedTextProcessor {
             try {
                 File file = new File(String.format("%s%s.txt", PATH, answer));
                 if (file.exists() && !file.isDirectory()) {
-                    //check reference existing
-                    if (pr.cash.get(answer) == null) {
-                        System.out.println("from file: ");
-                        StringJoiner sj = new StringJoiner(System.lineSeparator());
-                        Files.lines(Paths.get(String.format("%s%s.txt", PATH, answer))).forEach(line -> sj.add(line));
-                        String value = sj.toString();
-                        //create softReference as value for map
-                        SoftReference<String> sr = new SoftReference<>(value);
-                        pr.cash.put(answer, sr);
-                        result = value;
-                    } else {
-                        System.out.println("from cash: ");
-                        //get strong reference from soft
-                        result = pr.cash.get(answer).get();
-                    }
+                    result = cp.cacheText(answer);
                     System.out.println(result);
                 } else {
                     System.out.println("input correct name");
